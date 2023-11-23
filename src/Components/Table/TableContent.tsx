87e14790beb7a02f32942f04b486ac08412e179b
Luiz Henrique Pinheiro Silva <luizhenriquepinheirosilva@hotmail.com>
2023-11-22 23:55:19 -0300

    refactor type

--- src/Components/Table/TableContent.tsx
+++ src/Components/Table/TableContent.tsx
@@ -1,21 +1,28 @@
 import { Box, IconButton, Stack, Table, TableBody, TableCell, TableRow, useTheme } from '@mui/material';
-import React from 'react';
 
-export const TableContent = ({
+type RowType<T extends string | number | symbol> = {
+  [key in T]: string | number; // Adjust the type of the values as needed
+};
+
+export const TableContent =  <T extends string | number | symbol>({
   rows,
   rowsName,
   key,
   secondField,
   field,
+  rowCondition,
+  rowFieldCondition,
   fn,
   withoutBox,
 }: {
-  rows: any;
-  key?: string;
-  field: string;
-  rowsName: string;
-  secondField?: string;
+  rows: RowType<T>[];
+  key?: keyof RowType<T>;
+  field: keyof RowType<T>;
+  rowsName: keyof RowType<T>;
+  secondField?: keyof RowType<T>;
   withoutBox?: boolean;
+  rowCondition: string | number
+  rowFieldCondition: keyof RowType<T>
   fn: (selected: any) => void;
 }) => {
   const theme = useTheme();
@@ -38,30 +45,53 @@ export const TableContent = ({
               </TableCell>
               <TableCell align='right' padding='none'>
                 <Stack flexDirection={'row'} gap={2} justifyContent={'flex-end'} marginRight={2}>
-                  <IconButton
-                    sx={{
-                      width: 30,
-                      color: theme.palette.text.primary,
-                      height: 30,
-                    }}
-                    onClick={() => fn(row)}
+                  {row[rowFieldCondition] !== rowCondition && (
+                    <IconButton
+                      sx={{
+                        width: 30,
+                        color: theme.palette.text.primary,
+                        height: 30,
+                      }}
+                      onClick={() => fn(row)}
+                    >
+                      +
+                    </IconButton>
+                  )}
+                  {row[rowFieldCondition] === rowCondition && (
+                    <IconButton
+                      sx={{
+                        width: 30,
+                        color: theme.palette.text.primary,
+                        height: 30,
+                      }}
+                      onClick={() => fn(row)}
+                    >
+                      -
+                    </IconButton>
+                  )}
+                  <Box
+                    display={'flex'}
+                    justifyContent={'center'}
+                    alignItems={'center'}
+                    width={30}
+                    height={30}
+                    borderRadius={withoutBox ? 0 : 1}
+                    border={withoutBox ? 'none' : `1px solid ${theme.palette.secondary.main}`}
                   >
-                    +
-                  </IconButton>
+                    {row[field]}
+                  </Box>
                   {secondField && (
-                    <>
-                      <Box
-                        display={'flex'}
-                        justifyContent={'center'}
-                        alignItems={'center'}
-                        width={30}
-                        height={30}
-                        borderRadius={withoutBox ? 0 : 1}
-                        border={withoutBox ? 'none' : `1px solid ${theme.palette.secondary.main}`}
-                      >
-                        {row[secondField]}
-                      </Box>
-                    </>
+                    <Box
+                      display={'flex'}
+                      justifyContent={'center'}
+                      alignItems={'center'}
+                      width={30}
+                      height={30}
+                      borderRadius={withoutBox ? 0 : 1}
+                      border={withoutBox ? 'none' : `1px solid ${theme.palette.secondary.main}`}
+                    >
+                      {row[secondField]}
+                    </Box>
                   )}
                 </Stack>
               </TableCell>
